1d824b3b22f2f3ccc0c591e63ffa321b0a403ae8
Brain.B <vinhbao51299@gmail.com>
2021-06-28 23:27:02 +0700

    Update them apple

--- Snake.py
+++ Snake.py
@@ -3,19 +3,33 @@ from pygame.locals import *
 import pygame.gfxdraw
 import time
 
+size = 20
+
+class Apple:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.x = size * 4
+        self.y = size * 4
+    def draw(self):
+        pygame.gfxdraw.filled_circle(self.parent_screen, self.x, self.y, 7, [207, 4, 4])
+        pygame.display.flip()
+
 # Cấu hình cho con rắn
 class Snake:
-    def __init__(self, parent_screen ):
+    def __init__(self, parent_screen, length):
+        self.length = length
         self.parent_screen = parent_screen
-        self.y = 100
-        self.x = 100
+        self.y = [size]*length
+        self.x = [size]*length
+
         self.direction = 'down'
     # Hàm vẽ con rắn trong màn hình
     def draw(self):
         # Màu của màn hình
         self.parent_screen.fill((91, 162, 179))
-        # Hình dạng hộp con rắn
-        pygame.gfxdraw.box(self.parent_screen, [self.x, self.y, 20, 20], [163, 67, 33])
+        for i in range(self.length):
+            # Hình dạng hộp con rắn
+             pygame.gfxdraw.rectangle(self.parent_screen, [self.x[i], self.y[i], 18, 18], [115, 15, 111])
         # Cập nhật hay hiển thị lên màn hình
         pygame.display.flip()
 
@@ -31,14 +45,18 @@ class Snake:
 
     # Hàm này dùng để setup vị trí di chuyển của nó so với vị trí trước
     def walk(self):
+        for i in range(self.length-1, 0, -1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
+
         if self.direction == "up":
-            self.y -= 10
+            self.y[0] -= size
         if self.direction == "down":
-            self.y += 10
+            self.y[0] += size
         if self.direction == "left":
-            self.x -= 10
+            self.x[0] -= size
         if self.direction == "right":
-            self.x += 10
+            self.x[0] += size
 
         self.draw()
 
@@ -46,10 +64,16 @@ class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Game Snake")
-        self.surface = pygame.display.set_mode((1000, 500))
+        self.surface = pygame.display.set_mode((500, 500))
         self.surface.fill((91, 162, 179))
-        self.snake = Snake(self.surface)
+        self.snake = Snake(self.surface,7)
         self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
+
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
 
     def run(self):
         running = True
@@ -69,8 +93,8 @@ class Game:
                 elif event.type == QUIT:
                     running = False
 
-            self.snake.walk()
-            time.sleep(0.1)
+            self.play()
+            time.sleep(0.2)
 
 if __name__ == "__main__":
     a = Game()
